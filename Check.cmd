90e7056d0049654d6f453583d4e15f940762b182
Mariano <marianob85@gmail.com>
2018-01-14 11:26:31 +0100

    Modified   Jenkinsfile

--- Check.cmd
+++ Check.cmd
@@ -1,1 +1,1 @@
-python CheckSids.py -l "d:\Temp\E2_HD_settings_HB13E_HSWG_22122017 edit by bzyk83\lamedb" -i d:\Temp\goodsids.txt -o d:\Temp\Sids.txt
+python CheckSids.py -l "d:\Source\SvnDocuments\Settings\ChannelLists\Enigma2\lamedb" -i goodsids.txt -o Sids.txt
